4b717644fb619daf282cf929f96864579f8c1aba
takaaat <sasah0525@gmail.com>
2025-07-04 19:28:07 +0900

    refactor: collectionを使用

--- src/blog/post-1.md
+++ src/blog/post-1.md
@@ -1,5 +1,4 @@
 ---
-layout: "../../layouts/MarkdownPostLayout.astro"
 title: "My first blog post"
 pubDate: 2025-07-02
 description: "description"
---
+++ src/content.config.ts
@@ -0,0 +1,14 @@
+// Import the glob loader
+import { glob } from "astro/loaders";
+import { z, defineCollection } from "astro:content";
+const blog = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
+    schema: z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      category: z.string(),
+      tags: z.array(z.string())
+    })
+});
+export const collections = { blog };
