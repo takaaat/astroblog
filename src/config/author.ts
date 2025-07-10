6b67d44382e581ee17d9adad0885c5032df50fbc
takaaat <sasah0525@gmail.com>
2025-07-11 07:06:32 +0900

    refactor: configを参照するように

---
+++ src/config/author.ts
@@ -0,0 +1,5 @@
+export const authorConfig = {
+  name: "takaaat",
+  subtitle: "",
+  description: "このブログの記事を書いている人です",
+};
---
+++ src/config/site.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: "Astro",
+  blogName: "2not",
+  blogDescription: "2not blog",
+  copyright: "2025 2not blog. All rights reserved.",
+};
