5391d89d36c1672e4ceb17a1ec5f1f7f99073aa6
takaaat <sasah0525@gmail.com>
2025-07-11 06:55:45 +0900

    refactor: thumbnailの画像最適化処理

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,13 +3,13 @@ import { glob } from "astro/loaders";
 import { z, defineCollection } from "astro:content";
 const blog = defineCollection({
     loader: glob({ pattern: '**/index.md', base: "./src/blog" }),
-    schema: z.object({
+    schema: ({ image }) => z.object({
       title: z.string(),
       pubDate: z.date(),
       description: z.string(),
       category: z.string(),
       tags: z.array(z.string()),
-      thumbnail: z.string().optional()
+      thumbnail: image()
     })
 });
 export const collections = { blog };
